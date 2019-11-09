ed8f9721177d17bf228ff2287f4ece7307a5b5e2
Chema <chemaclass@outlook.es>
2019-11-09 14:17:27 +0100

    Add an example.php file

---
+++ example.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+use App\EdifactParser\EdifactParser;
+use App\EdifactParser\Segments\BGMBeginningOfMessage;
+use App\EdifactParser\Segments\CNTControl;
+use App\EdifactParser\Segments\DTMDateTimePeriod;
+use App\EdifactParser\Segments\MEADimensions;
+use App\EdifactParser\Segments\NADNameAddress;
+use App\EdifactParser\Segments\PCIPackageId;
+use App\EdifactParser\Segments\SegmentInterface;
+use App\EdifactParser\Segments\UNHMessageHeader;
+use App\EdifactParser\Segments\UNTMessageFooter;
+use EDI\Parser;
+
+require __DIR__ . '/bootstrap.php';
+
+$fileContent = <<<EDI
+UNA:+.? '
+UNB+UNOC:3+9457386:30+73130012:30+19101:118+8+MPM 2.19+1424'
+
+UNH+1+IFTMIN:S:93A:UN:PN001'
+BGM+340+56677786689+9'
+DTM+10:20191011:102'
+TSR+19+A4'
+CNT+7:0.51:KGM'
+CNT+11:1:PCE'
+RFF+CU:ValidationSet1'
+TDT+20'
+NAD+CZ+0410106314:160:Z12++Comany Returns Centre+c/o Carrier AB+City1++12345+DE'
+NAD+CN+++Person Name+Street Nr 2+City2++12345+DE'
+CTA+IC+:Person Name'
+COM+?+46980100:AL'
+COM+person.name@test.com:EM'
+GID+1+1'
+MEA+WT+G+KGM:0.62'
+MEA+VOL++MTQ:0'
+PCI+18+56677786689'
+UNT+18+1'
+
+UNH+2+IFTMIN:S:93A:UN:PN001'
+BGM+340+05055700896+9'
+DTM+10:20191011:102'
+TSR+19+A4'
+CNT+7:0.62:KGM'
+CNT+11:1:PCE'
+RFF+CU:ValidationSet2'
+TDT+20'
+NAD+CZ+0410106314:160:Z12++Comany Returns Centre+c/o Carrier AB+City1++12345+DE'
+NAD+CN+++Person Name+Street Nr 2+City2++12345+DE'
+CTA+IC+:Person Name'
+COM+?+46980100:AL'
+COM+person.name@test.com:EM'
+GID+1+1'
+MEA+WT+G+KGM:0.62'
+MEA+VOL++MTQ:0'
+PCI+18+05055700896'
+UNT+18+2'
+
+UNZ+2+8'
+EDI;
+
+$transactionResult = EdifactParser::parse(new Parser($fileContent));
+$firstMessage = $transactionResult->messages()[0];
+$segments = $firstMessage->segments();
+
+printSegment($segments[UNHMessageHeader::NAME]['1']);
+printSegment($segments[BGMBeginningOfMessage::NAME]['340']);
+printSegment($segments[DTMDateTimePeriod::NAME]['10']);
+printSegment($segments[CNTControl::NAME]['7']);
+printSegment($segments[CNTControl::NAME]['11']);
+printSegment($segments[NADNameAddress::NAME]['CZ']);
+printSegment($segments[MEADimensions::NAME]['WT']);
+printSegment($segments[MEADimensions::NAME]['VOL']);
+printSegment($segments[PCIPackageId::NAME]['18']);
+printSegment($segments[UNTMessageFooter::NAME]['18']);
+
+function printSegment(SegmentInterface $segment): void
+{
+    echo sprintf('%s - %s %s',$segment->name(),$segment->subSegmentKey(),PHP_EOL);
+}
--- tests/Functional/EdifactParserTest.php
+++ tests/Functional/EdifactParserTest.php
@@ -59,10 +59,6 @@ EDI;
         $unh = $segments[UNHMessageHeader::NAME]['1'];
         self::assertEquals(['UNH', '1', ['IFTMIN', 'S', '93A', 'UN', 'PN001']], $unh->rawValues());
 
-        /** @var UNTMessageFooter $unt */
-        $unt = $segments[UNTMessageFooter::NAME]['19'];
-        self::assertEquals(['UNT', '19', '1'], $unt->rawValues());
-
         /** @var CNTControl $cnt7 */
         $cnt7 = $segments[CNTControl::NAME]['7'];
         self::assertEquals(['CNT', ['7', '0.1', 'KGM']], $cnt7->rawValues());
@@ -70,5 +66,9 @@ EDI;
         /** @var CNTControl $cnt11 */
         $cnt11 = $segments[CNTControl::NAME]['11'];
         self::assertEquals(['CNT', ['11', '1', 'PCE']], $cnt11->rawValues());
+
+        /** @var UNTMessageFooter $unt */
+        $unt = $segments[UNTMessageFooter::NAME]['19'];
+        self::assertEquals(['UNT', '19', '1'], $unt->rawValues());
     }
 }
