15e829832e641ed8f2551e178928dfbd60fab5ac
Max Poliakov <48586126+Jaimies@users.noreply.github.com>
2022-10-30 18:20:32 +0100

    add CUX (Currency Details) segment

---
+++ src/Segments/CUXCurrencyDetails.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EdifactParser\Segments;
+
+/** @psalm-immutable */
+class CUXCurrencyDetails implements SegmentInterface
+{
+    public function __construct(private array $rawValues)
+    {
+    }
+
+    public function tag(): string
+    {
+        return self::class;
+    }
+
+    public function subId(): string
+    {
+        return $this->rawValues[1][0];
+    }
+
+    public function rawValues(): array
+    {
+        return $this->rawValues;
+    }
+}
---
+++ tests/Unit/Segments/CUXCurrencyDetailsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EdifactParser\Tests\Unit\Segments;
+
+use EdifactParser\Segments\CUXCurrencyDetails;
+use PHPUnit\Framework\TestCase;
+
+class CUXCurrencyDetailsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function segment_values(): void
+    {
+        $rawValues = ['CUX', ['5', 'GBP', '9']];
+        $segment = new CUXCurrencyDetails($rawValues);
+
+        self::assertEquals(CUXCurrencyDetails::class, $segment->tag());
+        self::assertEquals('5', $segment->subId());
+        self::assertEquals($rawValues, $segment->rawValues());
+    }
+}
