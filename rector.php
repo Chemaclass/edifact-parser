2248ccbde5c448a0d41ea61294149235cf411098
Chemaclass <chemaclass@outlook.es>
2025-05-29 11:56:56 +0200

    deps: install rector

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/example',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+     ->withPhpSets(php80: true)
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
